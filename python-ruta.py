eb9c5ed61f96f0caa57e3f217edb19d25b858a0a
jBujaico <74349643+jBujaico@users.noreply.github.com>
2024-02-15 02:01:17 -0500

    Update python-ruta.py

--- python-ruta.py
+++ python-ruta.py
@@ -1,9 +1,3 @@
+pip install pandas
 
-import os
-
-import pandas as pd
-# Seleccionando el directorio donde esta el chromedriver
-# Seleccionando el directorio donde esta el chromedriver
-os.chdir("C:/Users/hp/Desktop/Certus/dataops/sesion03/jenkins-python")
-os.getcwd()
 

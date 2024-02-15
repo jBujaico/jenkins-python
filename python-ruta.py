de4638543fad296b48673a531adc35ac39a61223
jBujaico <74349643+jBujaico@users.noreply.github.com>
2024-02-15 01:37:43 -0500

    Update python-ruta.py

--- python-ruta.py
+++ python-ruta.py
@@ -1,22 +1,9 @@
-import time
-import selenium
+
 import os
-from selenium import webdriver
-from selenium.webdriver.common.by import By 
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 import pandas as pd
 # Seleccionando el directorio donde esta el chromedriver
 # Seleccionando el directorio donde esta el chromedriver
 os.chdir("C:/Users/hp/Desktop/Certus/dataops/sesion03/jenkins-python")
 os.getcwd()
 
-# Crenado el objeto chrome_options
-chrome_options = webdriver.ChromeOptions()
-
-# Crear una instancia del controlador Chrome
-driver = webdriver.Chrome(options=chrome_options)
-# Maximizando
-driver.maximize_window()
-# Abriendo la pagina https://www.worldometers.info/world-population/population-by-country/
-driver.get("https://www.worldometers.info/world-population/population-by-country/")
